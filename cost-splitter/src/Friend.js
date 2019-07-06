ab4490c09ae17732c46b9532d918510e98ee4a64
vladmog <vladmog@gmail.com>
2019-07-06 04:01:40 -0700

    Formatted friend card, changed color of total calculation

--- cost-splitter/src/Friend.js
+++ cost-splitter/src/Friend.js
@@ -10,6 +10,39 @@ const FriendDiv = styled.div`
     padding-bottom: 10px;
 `;
 
+const NameDiv = styled.div`
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+`;
+
+const FiftyFiftyDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
+`;
+
+const ExpensesDiv = styled.div`
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+const CalculationsDiv = styled.div`
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+const TotalCostsDiv = styled.div`
+    color: blue;
+`;
+
+
 class Friend extends Component {
     state = {
         owesToggle: null,
@@ -36,33 +69,42 @@ class Friend extends Component {
         let friendOwes = parseFloat(this.props.evenPayment) - parseFloat(this.props.friend.totalCosts)
         return(
             <FriendDiv>
-                <h3>{this.props.friend.name}</h3>
-                {this.props.friend.cost1 === 0 ? (<></>) : (<div>{this.props.friend.expense1}: ${this.props.friend.cost1}</div>)}
-                {this.props.friend.cost2 === 0 ? (<></>) : (<div>{this.props.friend.expense2}: ${this.props.friend.cost2}</div>)}
-                {this.props.friend.cost3 === 0 ? (<></>) : (<div>{this.props.friend.expense3}: ${this.props.friend.cost3}</div>)}
-                {this.props.friend.cost4 === 0 ? (<></>) : (<div>{this.props.friend.expense4}: ${this.props.friend.cost4}</div>)}
-                {this.props.friend.cost5 === 0 ? (<></>) : (<div>{this.props.friend.expense5}: ${this.props.friend.cost5}</div>)}
-                <div>Total costs: ${this.props.friend.totalCosts}</div>
-                {(this.props.addingToggle
-                    ? <></>
-                    :this.state.evenToggle 
-                        ? (<div>You're even!</div>) 
-                        : (this.state.owesToggle 
-                            ? (this.props.owed.map(owed => {
-                                return (
-                                    <div key = {Math.random()}>
-                                        <Message 
-                                            friend = {this.props.friend} 
-                                            totalCosts = {this.props.friend.totalCosts}
-                                            evenPayment = {this.props.evenPayment}
-                                            friendOwes = {friendOwes}
-                                            owed = {owed}
-                                        />
-                                    </div>
-                                )
-                            })) 
-                            : (<div>You are owed ${(parseFloat(this.props.friend.totalCosts) - parseFloat(this.props.evenPayment)).toFixed(2)}</div>)
-                        ))}
+                <NameDiv>
+                    <h3>{this.props.friend.name}</h3>
+                </NameDiv>
+                <FiftyFiftyDiv>
+                    <ExpensesDiv>
+                        {this.props.friend.cost1 === 0 ? (<></>) : (<div>{this.props.friend.expense1}: ${this.props.friend.cost1}</div>)}
+                        {this.props.friend.cost2 === 0 ? (<></>) : (<div>{this.props.friend.expense2}: ${this.props.friend.cost2}</div>)}
+                        {this.props.friend.cost3 === 0 ? (<></>) : (<div>{this.props.friend.expense3}: ${this.props.friend.cost3}</div>)}
+                        {this.props.friend.cost4 === 0 ? (<></>) : (<div>{this.props.friend.expense4}: ${this.props.friend.cost4}</div>)}
+                        {this.props.friend.cost5 === 0 ? (<></>) : (<div>{this.props.friend.expense5}: ${this.props.friend.cost5}</div>)}
+                        <TotalCostsDiv>Total costs: ${this.props.friend.totalCosts}</TotalCostsDiv>
+                    </ExpensesDiv>
+                    {(this.props.addingToggle
+                        ? <></>
+                        :<CalculationsDiv>
+                            {this.state.evenToggle 
+                                ? (<div>You're even!</div>) 
+                                : (this.state.owesToggle 
+                                    ? (this.props.owed.map(owed => {
+                                        return (
+                                            <div key = {Math.random()}>
+                                                <Message 
+                                                    friend = {this.props.friend} 
+                                                    totalCosts = {this.props.friend.totalCosts}
+                                                    evenPayment = {this.props.evenPayment}
+                                                    friendOwes = {friendOwes}
+                                                    owed = {owed}
+                                                />
+                                            </div>
+                                        )
+                                    })) 
+                                    : (<div>You are owed ${(parseFloat(this.props.friend.totalCosts) - parseFloat(this.props.evenPayment)).toFixed(2)}</div>)
+                            )}
+                        </CalculationsDiv>
+                    )}
+                </FiftyFiftyDiv>
             </FriendDiv>
         )
     }
